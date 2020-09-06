3030f334d1d9f3db0e8e36554057c65ea5e92f63
HP <efafafaef>
2020-09-06 11:43:34 +0300

    feat: save task at LocalStorage

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-let content = document.querySelector('.content');
-let addTaskForm = document.forms.mainForm;
-let addTaskInput = addTaskForm.elements.task;
-let addTaskDateInput = addTaskForm.elements.date;
-let addTaskButton = document.querySelector('.add-button');
+const content = document.querySelector('.content');
+const addTaskForm = document.forms.mainForm;
+const addTaskInput = addTaskForm.elements.task;
+const addTaskDateInput = addTaskForm.elements.date;
+const addTaskButton = document.querySelector('.add-button');
 
-let changeDateForm = document.forms.modalDateForm;
-let changeDateInput = changeDateForm.elements.dateInput;
+const changeDateForm = document.forms.modalDateForm;
+const changeDateInput = changeDateForm.elements.dateInput;
 
-let modalConfirmWindow = document.querySelector('.modal-confirm');
-let modalAlertWindow = document.querySelector('.modal-alert');
-let modalChangeWindow = document.querySelector('.modal-change-date');
+const modalConfirmWindow = document.querySelector('.modal-confirm');
+const modalAlertWindow = document.querySelector('.modal-alert');
+const modalChangeWindow = document.querySelector('.modal-change-date');
+
+let todoList = localStorage.getItem('todo') ? JSON.parse(localStorage.getItem('todo')) : [];
+let doingList = localStorage.getItem('doing') ? JSON.parse(localStorage.getItem('doing')) : [];
+let doneList = localStorage.getItem('done') ? JSON.parse(localStorage.getItem('done')) : [];
+
+let id = localStorage.getItem('id') ? +localStorage.getItem('id') : 0;
+
+// localStorage.clear();
+
+//Отрисовка хранящихся в LocalStorage задач
+for (let item of todoList) {
+	document.querySelector('.todo-list').append(getTask(item));
+}
+
+for (let item of doingList) {
+	document.querySelector('.doing-list').append(getTask(item));
+}
+
+for (let item of doneList) {
+	document.querySelector('.done-list').append(getTask(item));
+}
 
 // Установка минимальной даты в поле ввода
 document.querySelectorAll('input[type=date]').forEach(item => {
 	item.min = new Date().toISOString().split('T')[0];
 });
 
-// Перевод даты введённой в поле, в формат dd.mm.yyyy
-function toLocalDate(input) {
-	let localDate = input.value.split('-');
+// Перевод даты в формат dd.mm.yyyy
+function toLocalDate(date) {
+	let localDate = date.split('-');
 	return localDate.reverse().join('.');
 }
 
@@ -26,27 +47,35 @@ function toLocalDate(input) {
 function fromLocalDate(date) {
 	let newDate = new Date();
 	let currentDate = date.split('.');
-	newDate.setFullYear(currentDate[2], currentDate[1], currentDate[0]);
+	newDate.setFullYear(currentDate[2], currentDate[1] - 1, currentDate[0]);
 	return newDate;
+}
+
+function fromLocalDateToString(date) {
+	return date.toISOString().split('T')[0];
 }
 
 // Сравнение двух дат
 function compareDate(date) {
 	let today = new Date();
 	let userDate = fromLocalDate(date);
-	userDate >= today ? true : false;
+	if(userDate >= today) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 // Проверка на пустые поля
 function isNoEmptyFields(object) {
 	for (let key in object) {
-		if (object[key].value === '') return false;
+		if (object[key] === '') return false;
 	}
 	return true;
 }
 
 // Получение количества задач и запись в заголовок блока
-function getTaskNumber(list) {
+function getTaskNumber(list) { 
 	return list.children.length;
 }
 
@@ -77,35 +106,46 @@ function createIconContainer() {
 }
 
 // Создание текстовой части новой задачи
-function createTaskTimeElement() {
+function createTaskTimeElement(time) {
 	let taskDate = document.createElement('p');
 	taskDate.classList.add('task__time');
-	taskDate.append(document.createTextNode(toLocalDate(addTaskDateInput)));
+	taskDate.append(document.createTextNode(toLocalDate(time)));
 	return taskDate;
 }
 
-function createTaskTextElement() {
+function createTaskTextElement(text) {
 	let taskText = document.createElement('p');
 	taskText.classList.add('task__text');
-	taskText.append(document.createTextNode(addTaskInput.value));
+	taskText.append(document.createTextNode(text));
 	return taskText;
 }
 
-function createTextContainer() {
+function createTextContainer(taskData) {
 	let container = document.createElement('div');
 	container.classList.add('text-container');
-	container.append(createTaskTimeElement());
-	container.append(createTaskTextElement());
+	container.append(createTaskTimeElement(taskData.date));
+	container.append(createTaskTextElement(taskData.task));
 	return container;
 }
 
 // Создать новую задачу
-function getTask() {
+function getTask(taskData) {
 	let task = document.createElement('li');
 	task.classList.add('task');
-	task.append(createTextContainer());
+	task.setAttribute('data-id', taskData.id);
+	task.append(createTextContainer(taskData));
 	task.append(createIconContainer());
 	return task;
+}
+
+// Получить данные из задачи
+function getTaskData(currentTask) {
+	let taskData = {
+		task: currentTask.firstElementChild.lastElementChild.innerHTML,
+		date: fromLocalDateToString(fromLocalDate(currentTask.firstElementChild.firstElementChild.innerHTML)),
+		id: currentTask.dataset.id,
+	}
+	return taskData;
 }
 
 // Открыть модальное окно для подтверждения удаления
@@ -142,7 +182,7 @@ function openModalChangeDateWindow() {
 				if (changeDateInput.value === '') {
 					changeDateInput.classList.add('empty-field');
 				} else {
-					resolve(toLocalDate(changeDateInput));
+					resolve(toLocalDate(changeDateInput.value));
 				}
 			}
 			if (event.target.classList.contains('button__cancel')) {
@@ -162,13 +202,20 @@ function closeModalWindow(window) {
 /*---------------------------------------------Events------------------------------*/
 // Добавление задачи
 addTaskButton.addEventListener('click', function () {
+	id++;
+	localStorage.setItem('id', `${id}`);
+
 	let newTask = {
-		task: addTaskInput,
-		date: addTaskDateInput,
+		task: addTaskInput.value,
+		date: addTaskDateInput.value,
+		id: id,
 	}
+
 	let check = isNoEmptyFields(newTask);
 	if (check) {
-		document.querySelector('.todo-list').append(getTask());
+		todoList.push(newTask);
+		addTaskListToStorage('todo', todoList);
+		document.querySelector('.todo-list').append(getTask(newTask));
 		addTaskForm.reset();
 	} else {
 		document.querySelectorAll('input').forEach((item) => {
@@ -180,6 +227,10 @@ addTaskButton.addEventListener('click', function () {
 	}
 	createTaskNumber(document.querySelector('.todo-board').querySelector('h2').firstElementChild);
 });
+
+function addTaskListToStorage(listName, taskList) {
+	localStorage.setItem(`${listName}`, JSON.stringify(taskList));
+}
 
 // Выделение незаполненного обязательного поля
 document.querySelectorAll('.required-input').forEach(item => {
@@ -207,7 +258,7 @@ content.addEventListener('click', function(event) {
 });
 
 function deleteTask(task) {
-	let title = task.closest('.board').querySelector('h2').firstElementChild
+	let title = task.closest('.board').querySelector('h2').firstElementChild;
 	if(task.closest('.board').classList.contains('doing-board')){
 		openModalConfirmWindow()
 		.then(() => {
@@ -249,25 +300,49 @@ content.addEventListener('click', function (event) {
 		return;
 	}
 	let board = event.target.closest('.board');
-	if(board.classList.contains('todo-board') && board.parentElement.querySelector('.doing-list').children.length === 5) {
-		openModalAlertWindow();
-	} else if (board.classList.contains('done-board')) {
+	let task = event.target.closest('.task');
+
+	if(board.classList.contains('todo-board')) {
+		if(content.querySelector('.doing-list').children.length === 5) {
+			openModalAlertWindow();
+		} else {
+			removeTask(task, board);
+			let currentTaskIndex = todoList.findIndex(item => {
+				item.id === task.dataset.id;
+			});
+			todoList.splice(currentTaskIndex, 1);
+			doingList.push(getTaskData(task));
+			addTaskListToStorage('todo', todoList);
+			addTaskListToStorage('doing', doingList);
+		}
+	} else if(board.classList.contains('done-board')) {
 		openModalChangeDateWindow()
 		.then(resolve => {
-			removeTaskFromDoneAndChangeDate(resolve, event.target.closest('.task'));
+			removeTaskFromDoneAndChangeDate(resolve, task);
 		})
 		.catch(reject => {
-			removeTaskFromDoneAndChangeDate(reject, event.target.closest('.task'));
+			removeTaskFromDoneAndChangeDate(reject, task);	
 		});
 	} else {
-		board.nextElementSibling.querySelector('.list').append(event.target.closest('.task'));
-		createTaskNumber(board.querySelector('h2').firstElementChild);
-		createTaskNumber(board.nextElementSibling.querySelector('h2').firstElementChild);
+		removeTask(task, board);
+		let currentTaskIndex = doingList.findIndex(item => {
+			item.id === task.dataset.id;
+		});
+		doingList.splice(currentTaskIndex, 1);
+		doneList.push(getTaskData(task));
+		addTaskListToStorage('doing', doingList);
+		addTaskListToStorage('done', doneList);
 	}
 });
 
+function removeTask(task, board) {
+	board.nextElementSibling.querySelector('.list').append(task);
+	createTaskNumber(board.querySelector('h2').firstElementChild);
+	createTaskNumber(board.nextElementSibling.querySelector('h2').firstElementChild);
+}
+
 function removeTaskFromDoneAndChangeDate(result, task) {
-	let todoBoardTitle = task.closest('.content').querySelector('.todo-board').querySelector('h2').firstElementChild;
+	let todoBoardTitle = content.querySelector('.todo-board').querySelector('h2').firstElementChild;
 	let currentBoardTitle = task.closest('.board').querySelector('h2').firstElementChild;
 	if (!compareDate(task.querySelector('.task__time').innerHTML) && !result) {
 		let today = new Date().toISOString().split('T')[0].split('-').reverse().join('.');
@@ -275,10 +350,17 @@ function removeTaskFromDoneAndChangeDate(result, task) {
 	} else if(result) {
 		task.querySelector('.task__time').innerHTML = result;
 	}
-	task.closest('.content').querySelector('.todo-list').append(task);
+	content.querySelector('.todo-list').append(task);
 	createTaskNumber(currentBoardTitle);
 	createTaskNumber(todoBoardTitle);
 	closeModalWindow(modalChangeWindow);
+	let currentTaskIndex = doneList.findIndex(item => {
+		item.id === task.dataset.id;
+	});
+	doneList.splice(currentTaskIndex, 1);
+	todoList.push(getTaskData(task));
+	addTaskListToStorage('todo', todoList);
+	addTaskListToStorage('done', doneList);
 }
 
 // Закрытие модального окна с предупреждением о необходимости выполнить что-то, прежде чем добавлять шестую задачу
